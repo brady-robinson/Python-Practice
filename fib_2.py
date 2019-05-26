4b41ff7079cdd45c57f3c7a1317ab54036dab6d5
Brady Robinson <bradyrobinson3@gmail.com>
2019-05-26 09:41:49 -0400

    Add files to python_practice

--- fib_2.py
+++ fib_2.py
@@ -1,4 +1,4 @@
-def fib(n):
+def fib_print(n):
     """Print a Fibonaci series up to n."""
     a, b = 0, 1
     while a < n:
@@ -6,4 +6,23 @@ def fib(n):
         a, b = b, a+b
     print()
 
-fib(2000)
+fib_print(2000)
+
+print(fib_print)
+
+f = fib_print
+
+f(100)
+
+fib_print(0)
+
+
+def fib_return(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+print(fib_return(100))
