c7d2169e1095e1bcdb9f268cfc90a2c6f5c30a9a
Brady Robinson <bradyrobinson3@gmail.com>
2019-05-25 21:18:19 -0400

    Add files to python_practice

---
+++ range.py
@@ -0,0 +1,16 @@
+# for i in range(1, 6):
+#     print(i)
+
+# for i in range(1, 6, 2):
+#     print(i)
+
+# for i in range(-10, -100, -30):
+#     print(i)
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
+
+
+print(range(10))
+print(list(range(10)))
